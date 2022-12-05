7655ab01c1297632de684bfd8a76c2d5b2560e98
Lucas Gabriel <lucasgabriel20032013@gmail.com>
2022-12-05 00:09:51 -0300

    Co-authored-by: Marcos Gabriel <ForrageadorXD@users.noreply.github.com>

--- Site/urls.py
+++ Site/urls.py
@@ -20,4 +20,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",index, name='index')
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
