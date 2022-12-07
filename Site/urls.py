e5d3ee422334956d0f3a61f34f4464b7d6a80be5
Forrageador <forrageador@gmail.com>
2022-12-07 04:24:52 -0300

    Tela de login

--- Site/urls.py
+++ Site/urls.py
@@ -18,11 +18,13 @@ from django.urls import path
 from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",index, name='index'),
     path('cadastro/', cadastro, name='cadastro'),
     path('login/',login, name='login'),
-    path('plataforma',plataforma, name='plataforma')
+    path('plataforma',plataforma, name='plataforma'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
